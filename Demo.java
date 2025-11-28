c4f7ce06e61d7838f2c5b3f70b7890e5ac897ef7
Lubdhesh Rathor <140482383+lubdhesh18@users.noreply.github.com>
2025-11-28 08:52:27 +0530

    Update webhook message in Demo.java

--- Demo.java
+++ Demo.java
@@ -2,10 +2,11 @@ class Demo
 {
 	public static void main(String[] args) {
 		System.out.println("Hii It's Lubdhesh");
-		System.out.println("Web hook triggered ");
+		System.out.println("GitHub webhook triggered ");
 	}
 
 }
+
 
 
 
