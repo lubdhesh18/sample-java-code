ebd8912bd871cfb29d9fed55dce968a4b17c1407
Lubdhesh Rathor <140482383+lubdhesh18@users.noreply.github.com>
2025-11-28 08:42:08 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,10 +2,11 @@ class Demo
 {
 	public static void main(String[] args) {
 		System.out.println("Hii It's Lubdhesh");
-		System.out.println("Web hook os triggered");
+		System.out.println("Web hook os triggered ");
 	}
 
 }
+
 
 
 
